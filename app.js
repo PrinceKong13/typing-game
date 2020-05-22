1179150f87d85faca435ed9595d4aaf1081e1a1d
Will <wk1394@gmail.com>
2020-05-22 12:28:41 -0500

    highlighter now moves appropriately and the sentence updates correctly

--- app.js
+++ app.js
@@ -1,13 +1,38 @@
 let lastKeyPress;
+let currentSentence = 0;
+let sentences = [
+  "ten ate neite ate nee enet ite ate inet ent eate",
+  "Too ato too nOt enot one totA not anot tOO aNot",
+  "oat itain oat tain nate eate tea anne inant nean",
+  "itant eate anot eat nato inate eat anot tain eat",
+  "nee ene ate ite tent tiet ent ine ene ete ene ate",
+];
+let sentenceLength = sentences[currentSentence].length;
+let charSelector = 0;
+let currentLetter = sentences[currentSentence][charSelector];
 
-//Hides the uppercase keyboard
 $("#keyboard-upper-container").css("display", "none");
+$("#target-letter").text(currentLetter);
+$("#sentence").append(highLightAt(charSelector));
 
-//Displays the uppercase keyboard on pressing shift and hides the lower case one
+//Everything in the game runs based on key presses, so most things happen right here
 $(document).keydown(function (event) {
+
+  //Shows uppercase keyboard when shift is pressed
   if (event.which == 16) {
     $("#keyboard-upper-container").css("display", "");
     $("#keyboard-lower-container").css("display", "none");
+    
+  } else if (charSelector == sentenceLength - 1) {
+    newSentence();
+
+    //Moves the highlight and changes current target letter
+  } else {
+    ++charSelector;
+    currentLetter = sentences[currentSentence][charSelector];
+    $("#target-letter").text(currentLetter);
+    $("#sentence").empty();
+    $("#sentence").append(highLightAt(charSelector));
   }
 });
 
@@ -20,7 +45,31 @@ $(document).keyup(function (event) {
   $(".key").css("backgroundColor", "");
 });
 
+//Highlights keys when pressed and stores the ascii value for the pressed key
 $(document).keypress(function (event) {
   lastKeyPress = event.which;
   $("#" + lastKeyPress).css("backgroundColor", "gray");
 });
+
+//This highlights the target letter in the sentence
+function highLightAt(index) {
+  return (
+    sentences[currentSentence].substr(0, index) +
+    "<span class='highlighted'>" +
+    sentences[currentSentence].substr(index, 1) +
+    "</span>" +
+    sentences[currentSentence].substr(index + 1, sentenceLength)
+  );
+}
+
+//This resets the prompt and needed variables for a new sentence
+function newSentence() {
+  ++currentSentence;
+  charSelector = 0;
+  $("#sentence").empty();
+  $("#sentence").append(highLightAt(charSelector));
+  currentLetter = sentences[currentSentence][charSelector];
+  $("#target-letter").text(currentLetter);
+  sentenceLength = sentences[currentSentence].length;
+
+}
