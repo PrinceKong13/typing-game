5e97141b52a2d2bbb97d00f55de556256a811201
Will <wk1394@gmail.com>
2020-05-28 09:39:11 -0500

    organized code for readability

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ let gameState = {
   currentSentence: 0,
   charSelector: 0,
   numberOfMistakes: 0,
-  startTime: Date.now()
-}
+  startTime: Date.now(),
+};
 let sentences = [
   "ten ate neite ate nee enet ite ate inet ent eate",
   "Too ato too nOt enot one totA not anot tOO aNot",
@@ -35,52 +35,15 @@ function startGame() {
 
     //Everything in the game runs based on key presses, so most things happen right here
     $(document).keydown(function (event) {
-      gameState.lastKeyPress = event.key.charCodeAt(0);
-      console.log(gameState.lastKeyPress);
-      $("#" + gameState.lastKeyPress).css("backgroundColor", "gray");
-      //Shows uppercase keyboard when shift is pressed
+      highlightPressedKey();
       if (event.keyCode == 16) {
-        $("#keyboard-upper-container").css("display", "");
-        $("#keyboard-lower-container").css("display", "none");
-
-        //This sets up a new sentence when the previous one is complete or ends the game if the last sentence is completed
+        showUpperCase();
       } else if (gameState.charSelector == sentenceLength - 1) {
-        ++gameState.currentSentence;
-        if (gameState.currentSentence == 5) {
-          endGame();
-        } else {
-          gameState.charSelector = 0;
-          $("#sentence").empty();
-          $("#sentence").append(highlightAt(gameState.charSelector));
-          currentLetter = sentences[gameState.currentSentence][gameState.charSelector];
-          $("#target-letter").text(currentLetter);
-          sentenceLength = sentences[gameState.currentSentence].length;
-          $("#feedback").empty();
-        }
-
-        //This puts a feedback icon in depending on whether the user hit the right key, it also tracks the number of mistakes and moves the highlighter and target letter
+        updateSentence();
       } else {
-        if (event.key == currentLetter) {
-          $("#feedback").append("<span class='glyphicon glyphicon-ok'></span>");
-        } else {
-          $("#feedback").append(
-            "<span class='glyphicon glyphicon-remove'></span>"
-          );
-          ++gameState.numberOfMistakes;
-        }
-        ++gameState.charSelector;
-        currentLetter = sentences[gameState.currentSentence][gameState.charSelector];
-        $("#target-letter").text(currentLetter);
-        $("#sentence").empty();
-        $("#sentence").append(highlightAt(gameState.charSelector));
+        feedback();
       }
     });
-
-    //Highlights keys when pressed and stores the ascii value for the pressed key
-    // $(document).keypress(function (event) {
-    //   gameState.lastKeyPress = event.which;
-    //   $("#" + gameState.lastKeyPress).css("backgroundColor", "gray");
-    // });
 
     //Hides the upper case keyboard once shift is released and returns the lower case one. Also returns keys to their original color when released
     $(document).keyup(function (event) {
@@ -114,8 +77,8 @@ function endGame() {
   $("#target-letter").empty();
   $("#sentence").text(
     "Game Over! Your score is: " +
-      ((54 / minutes) - (2 * gameState.numberOfMistakes)) +
-      " Words Per Minutes!"
+    (54 / minutes - 2 * gameState.numberOfMistakes) +
+    " Words Per Minutes!"
   );
   $("#target-letter").append("<button id='restart'>Restart</button>");
   $("#restart").click(function () {
@@ -123,4 +86,49 @@ function endGame() {
     $("#target-letter").empty();
     startGame();
   });
+}
+
+//This highlights a key when it is pressed
+function highlightPressedKey() {
+  gameState.lastKeyPress = event.key.charCodeAt(0);
+  $("#" + gameState.lastKeyPress).css("backgroundColor", "gray");
+}
+
+//Shows the uppercase keyboard
+function showUpperCase() {
+  $("#keyboard-upper-container").css("display", "");
+  $("#keyboard-lower-container").css("display", "none");
+}
+
+//Replaces the current sentence with the next one when completed or ends the game if the last sentence is completed
+function updateSentence() {
+  ++gameState.currentSentence;
+  if (gameState.currentSentence == 5) {
+    endGame();
+  } else {
+    gameState.charSelector = 0;
+    $("#sentence").empty();
+    $("#sentence").append(highlightAt(gameState.charSelector));
+    currentLetter = sentences[gameState.currentSentence][gameState.charSelector];
+    $("#target-letter").text(currentLetter);
+    sentenceLength = sentences[gameState.currentSentence].length;
+    $("#feedback").empty();
+  }
+}
+
+//Tracks mistakes and provides feedback based on whether the player hit the right key. Also moves the highlighter based on what letter is currently required
+function feedback() {
+  if (event.key == currentLetter) {
+    $("#feedback").append("<span class='glyphicon glyphicon-ok'></span>");
+  } else {
+    $("#feedback").append(
+      "<span class='glyphicon glyphicon-remove'></span>"
+    );
+    ++gameState.numberOfMistakes;
+  }
+  ++gameState.charSelector;
+  currentLetter = sentences[gameState.currentSentence][gameState.charSelector];
+  $("#target-letter").text(currentLetter);
+  $("#sentence").empty();
+  $("#sentence").append(highlightAt(gameState.charSelector));
 }
